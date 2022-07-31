142991c44cef1abaadad8b9d6ad3554d08aaa3f6
DelinM <delinmuray@gmail.com>
2022-07-31 19:07:15 -0400

    Update lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,11 +3,11 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Return next number */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 != 0) {
             return 3 * n + 1;
         } else {
             return n / 2;
