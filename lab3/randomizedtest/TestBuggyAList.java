dc0d4119b9df84b1918f6bd0e340ff13d45415b9
DelinM <delinmuray@gmail.com>
2022-09-19 13:02:13 -0400

    update

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -20,36 +20,42 @@ public class TestBuggyAList {
         }
 
         assertEquals(alist.size(), blist.size());
-
         assertEquals(alist.removeLast(), blist.removeLast());
+
         assertEquals(alist.removeLast(), blist.removeLast());
         assertEquals(alist.removeLast(), blist.removeLast());
     }
     @Test
     public static void randomizedTest() {
         AListNoResizing<Integer> L = new AListNoResizing<>();
-        
+        BuggyAList<Integer> K = new BuggyAList();
 
-        int N = 500;
+
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 3);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
+                K.addLast(randVal);
                 System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
                 int size = L.size();
+                int size_2 = K.size();
                 System.out.println("size: " + size);
+                assertEquals(size, size_2);
             } else if (operationNumber == 2 & L.size() > 0) {
-                int number = L.getLast();
+                int number_1 = L.getLast();
                 L.removeLast();
-                System.out.println("removeLast(" + number + ")");
+                int number_2 = K.getLast();
+                K.removeLast();
+                System.out.println("removeLast(" + number_1 + ")");
+                assertEquals(number_1, number_2);
             }
         }
     }
-
     public static void main(String[] args) {
         randomizedTest();
     }
