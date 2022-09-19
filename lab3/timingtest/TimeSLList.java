23b07e678c63a739bd8db61e5f1f1f37754cd556
DelinM <delinmuray@gmail.com>
2022-09-19 09:25:58 -0400

    Update TimeSLList.java

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,6 +24,20 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        SLList<Integer> sllist = new SLList<>();
+        int tick = 1;
+
+        for (int i = 0; i < 128000; i ++) {
+            sllist.addLast(i);
+            if (i == Math.pow(2, tick)) {
+
+            }
+        }
+
     }
 
 }
