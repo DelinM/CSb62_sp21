5245a2d8b37f30c1c9f09c140c26257ea1dce474
DelinM <delinmuray@gmail.com>
2022-10-04 20:43:38 -0400

    update LLD

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,8 +1,66 @@
 package deque;
 
-public class LinkedListDeque<Blorp>{
-    private
+public class LinkedListDeque<T> {
 
+    private class IntNode() {
+        public IntNode prev;
+        public T item;
+        public IntNode next;
 
+        public IntNode(IntNode p, T i, IntNode n) {
+            prev = p;
+            item = i;
+            next = n;
+    }
 
+    private IntNode sentinel;
+    public int size;
+    
+    public LinkedListDeque(T item) {
+        
+        size = 1;
+
+    }
+
+    
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void printDeque() {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T get(int index) {
+
+    }
+
+    public Iterator<T> iterator() {
+
+    }
+
+    public boolean equals(Object o) {
+
+    }
 }
+
