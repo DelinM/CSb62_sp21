4c6cd0fa7c20ed3cf0fd3731de9412c363052355
DelinM <delinmuray@gmail.com>
2022-10-05 23:04:35 -0400

    update

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -11,66 +11,85 @@ public class LinkedListDeque<T> {
             prev = p;
             item = i;
             next = n;
+        }
     }
 
     /* first item is at sentinel.next */
     private StuffNode sentinel;
     public int size;
-    
-    /** Creats an empty LLD */
+
+    /**
+     * Creats an empty LLD
+     */
     public LinkedListDeque() {
         sentinel = new StuffNode(null, null, null);
         size = 0;
     }
 
-    /** Create an LLD with new data */
+    /**
+     * Create an LLD with new data
+     */
     public LinkedListDeque(T x) {
         sentinel = new StuffNode(null, null, null);
         sentinel.next = new StuffNode(sentinel, x, sentinel);
+        sentinel.prev = sentinel.next;
         size = 1;
     }
 
-    
+    /** Add an element at location one */
     public void addFirst(T x) {
         sentinel.next = new StuffNode(sentinel, x, sentinel.next);
-        first.prev = new StuffNode(null, x, first);
+        sentinel.next.next.prev = sentinel.next;
         size++;
-
     }
 
+    /** Add an element at location last */
     public void addLast(T x) {
-        IntNode p = sentinel;
-
-        while (p.next != null) {
-            p = p.next;
-        }
-        p.next = new IntNode(p, x, sentinel);
+        sentinel.prev.next = new StuffNode(sentinel.prev, x, sentinel);
         size++;
-
     }
 
     public boolean isEmpty() {
-        if (size = 0){
-            return True;
+        if (size == 0) {
+            return true;
         } else {
-            return False;
+            return false;
         }
-
     }
 
     public int size() {
         return size;
-
     }
 
     public void printDeque() {
-        
+        StuffNode p = sentinel.next;
+
+        while(p.item != null) {
+            System.out.println(p.item);
+            p = p.next;
+        }
+
     }
 
     public T removeFirst() {
-
+        T i = sentinel.next.item;
+        if (sentinel.prev.equals(sentinel.next)) {
+            sentinel = new StuffNode(null, null, null);
+        } else {
+            sentinel.next = sentinel.next.next;
+            sentinel.next.next.prev = sentinel;
+        }
+    return i;
     }
 
+    public static void main(String[] args) {
+        LinkedListDeque<String> lld1 = new LinkedListDeque<>("something1");
+        lld1.addFirst("something2");
+        lld1.addFirst("something3");
+        lld1.addFirst("something3");
+        lld1.addLast("something4");
+        System.out.println("2");
+    }
     public T removeLast() {
 
     }
@@ -79,16 +98,13 @@ public class LinkedListDeque<T> {
 
     }
 
-    public Iterator<T> iterator() {
+//    public Iterator<T> iterator() {
+//
+//    }
+//
+//    public boolean equals(Object o) {
+//
+//    }
 
-    }
 
-    public boolean equals(Object o) {
-
-    }
-
-    public static void main(String[] args){
-
-    }
 }
-
