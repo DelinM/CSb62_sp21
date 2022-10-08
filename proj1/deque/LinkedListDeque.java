d0cbc4ef22f9ad140b46e3c51abe49a859971203
DelinM <delinmuray@gmail.com>
2022-10-07 23:26:58 -0400

    Update LinkedListDeque.java

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -5,67 +5,123 @@ public class LinkedListDeque<T> {
     public StuffNode sentinel;
     public int size;
 
-
     private class StuffNode {
 
-        public stuf
+        public StuffNode prev;
+        public T item;
+        public StuffNode next;
+
+        public StuffNode(StuffNode p, T i, StuffNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
 
     }
 
-
-    /**
-     * Creats an empty LLD
-     */
+    /** Creats an empty LLD */
     public LinkedListDeque() {
+        sentinel = new StuffNode(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+
     }
 
-    /**
-     * Create an LLD with new data
-     */
+    /** Create an LLD with new data */
     public LinkedListDeque(T x) {
+        sentinel = new StuffNode(null, null, null);
+        sentinel.next = new StuffNode(sentinel, x, sentinel);
+        sentinel.prev = sentinel.next;
+        size = 1;
     }
 
     /** Add an element at location one */
     public void addFirst(T x) {
+        sentinel.next = new StuffNode(sentinel, x, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+
+        /** increase size by 1 */
+        size++;
     }
 
     /** Add an element at location last */
     public void addLast(T x) {
+        sentinel.prev = new StuffNode(sentinel.prev, x, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
 
+        /** increase size by 1 */
+        size++;
     }
 
     public boolean isEmpty() {
-
-
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int size() {
-
+        return size;
     }
 
     public void printDeque() {
-
-
+        StuffNode p = sentinel.next;
+        for (int i = 1; i <= size; i++) {
+            System.out.println(p.item);
+            p = p.next;
+        }
     }
 
     public T removeFirst() {
 
+        if (size == 0) {
+            return null;
+        }
+        /** get the value of T*/
+        StuffNode p = sentinel.next;
+        T result = p.item;
+
+        /** reassign stuffnode*/
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return result;
     }
 
-    public static void main(String[] args) {
-        LinkedListDeque<String> lld1 = new LinkedListDeque<>();
-        lld1.addFirst("something2");
-        lld1.addFirst("something3");
-        lld1.addFirst("something3");
-        lld1.addLast("something4");
-        System.out.println("2");
-    }
     public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
 
+        /** get the last item*/
+        StuffNode p = sentinel.prev;
+        T result = p.item;
+
+        /** remove the last node */
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+
+        size--;
+
+        return  result;
     }
 
     public T get(int index) {
 
+        /** base condition check*/
+        if (size == 0 || index > size - 1 ) {
+            return null;
+        }
+
+        /** note that when it comes to size and index, size is always 1 bigger than index*/
+        StuffNode p = sentinel;
+        for (int i = 0;i <= index;i++ ) {
+            p = p.next;
+        }
+
+        return p.item;
     }
 
 //    public Iterator<T> iterator() {
@@ -75,6 +131,5 @@ public class LinkedListDeque<T> {
 //    public boolean equals(Object o) {
 //
 //    }
-
 
 }
