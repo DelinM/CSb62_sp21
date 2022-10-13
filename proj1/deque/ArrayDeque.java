9037b9180b44e25912c7605b80b6b30bbdb4f59b
DelinM <delinmuray@gmail.com>
2022-10-13 18:02:17 -0400

    Update ArrayDeque.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,44 +1,59 @@
 package deque;
 
-
-
 public class ArrayDeque<T> {
     public int size;
     private int nextFirst;
     private int nextLast;
     public T[] items;
 
-    /** instantiate ArrayDeque with no input*/
     public ArrayDeque() {
 
-        /** instantaite deque */
         items = (T[]) new Object[8];
         size = 0;
         nextFirst = 4;
         nextLast = 5;
     }
 
-    /** resize method required when adding */
     public void addFirst(T item) {
         size++;
 
+        // if nextFirst reach 0, assign it to items.length - 1;
+
+        if (nextFirst == -1) {
+            nextFirst = items.length - 1;
+        }
+
         items[nextFirst] = item;
         nextFirst = nextFirst - 1;
-
-
     }
 
     public void addLast(T item) {
         size++;
 
+        // if nextLast is longer than the length - 1, assign it to 0
+        if(nextLast > items.length - 1) {
+            nextLast = 0;
+        }
+        items[nextLast] = item;
+        nextLast = nextLast + 1;
+    }
 
+    private boolean checkResize(int nFirst, int nLast) {
+        if ((nFirst - nLast) == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    private void adResize() {
 
     }
 
-    private 
-
     public boolean isEmpty() {
-
+        if (size == 0) {
+            return true;
+        }
+        return false;
     }
 
     public int size() {
@@ -47,18 +62,36 @@ public class ArrayDeque<T> {
 
     public void printDeque() {
 
-    }
+        for(int i = nextFirst+1; i < items.length; i ++) {
+            System.out.println(items[i]);
+        }
 
-    public T removeFirst() {
 
-    }
-
-    public T removeLast() {
 
     }
+//
+//    public T removeFirst() {
+//
+//    }
+//
+//    public T removeLast() {
+//
+//    }
+//
+//    public T get(int index) {
+//
+//    }
 
-    public T get(int index) {
-
+    public static void main(String[] args) {
+        ArrayDeque test = new ArrayDeque();
+        test.addFirst(5);
+        test.addFirst(3);
+        test.addFirst(4);
+        test.addFirst(6);
+        test.addFirst(9);
+        test.addLast(10);
+        test.addFirst(2);
+        test.addFirst(2);
     }
 
 }
